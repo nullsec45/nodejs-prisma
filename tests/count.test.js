b0ef6201e424945109257c5e553d59c908e72dd9
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-18 22:58:05 +0700

    test : change name to Enrtong in count

--- tests/count.test.js
+++ tests/count.test.js
@@ -1,15 +1,13 @@
 const { prismaClient } = require("../src/prisma-client");
 
 describe("Prisma Client", () => {
-    it("should can count", async () => {
-        const total = await prismaClient.customer.count({
-            where: {
-                name: "Fajar"
-            }
-        });
-
-        expect(total).toBe(1);
+  it("should can count", async () => {
+    const total = await prismaClient.customer.count({
+      where: {
+        name: "Entong Surotong",
+      },
     });
 
-
+    expect(total).toBe(1);
+  });
 });
